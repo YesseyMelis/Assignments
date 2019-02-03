4f87c2414c4eea6f7d4b036f4f9608cb051fe7e4
Yessey Melis <170107028@stu.sdu.edu.kz>
2019-02-03 21:41:03 +0600

    rewrite aibols js add events

--- secondAssignment/aibol.js
+++ secondAssignment/aibol.js
@@ -4,15 +4,25 @@ elementsArray.forEach(function(elem) {
         var name = this.querySelector('.name').innerHTML;
         var src = this.querySelector('.photo').querySelector('img').src;
         var price = this.querySelector('.price').innerHTML;
-        var info = document.getElementById("info");
-        info.style.display = "block";
+        var inf = document.getElementById("info");
+        if(document.querySelector('#info').classList.contains("close")){
+            document.querySelector('#info').classList.remove("close");
+            document.querySelector('#info').classList.add("open");
+            document.querySelector('.content').classList.remove("contentWhenClosed");
+            document.querySelector('.content').classList.add("contentWhenOpened");
+        }else {
+            document.querySelector('#info').classList.remove("open");
+            document.querySelector('#info').classList.add("close");
+            document.querySelector('.content').classList.remove("contentWhenOpened");
+            document.querySelector('.content').classList.add("contentWhenClosed");
+        }
         console.log(info);
         info.querySelector('.hoverme').querySelector('img').src = src
         info.querySelector('.infoContent').querySelector('.name_info').innerHTML = name
         info.querySelector('.infoContent').querySelector('.price_info').innerHTML = price
-        
     });
 });
+
 function sort(){
     var selected = document.getElementById("selectedSort");
     var sortType = selected.options[selected.selectedIndex].text;
